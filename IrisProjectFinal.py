a99bf9d668bdc3f4daf5a8fba05efbaa58f37cd8
Ereen <154412749+minicooperdaihatsu@users.noreply.github.com>
2023-12-23 03:33:22 +0800

    Update IrisProjectFinal.py

--- IrisProjectFinal.py
+++ IrisProjectFinal.py
@@ -7,18 +7,22 @@ with open('iris_model_gnb.pkl', 'rb') as file:
 
 # Streamlit app
 def main():
-    st.title("Iris Flower Prediction App (Gaussian Naive Bayes)")
+    st.title("# Simple Iris Flower Prediction App")
+    st.write("This app predicts the **Iris flower** type!")
 
+st.sidebar.header('User Input Parameters')
     # Feature sliders for input values
-    sepal_length = st.slider("Sepal Length", 4.0, 8.0, 5.0)
-    sepal_width = st.slider("Sepal Width", 2.0, 4.5, 3.0)
-    petal_length = st.slider("Petal Length", 1.0, 7.0, 4.0)
-    petal_width = st.slider("Petal Width", 0.1, 2.5, 1.0)
+def user_input_features():
+    sepal_length = st.sidebar.slider("Sepal Length", 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider("Sepal Width", 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider("Petal Length", 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider("Petal Width", 0.1, 2.5, 0.2)
 
     # Predict the target class
     input_data = [[sepal_length, sepal_width, petal_length, petal_width]]
     prediction = model.predict(input_data)[0]
-
+    return prediction
+    
     # Display the prediction
     species_mapping = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
     st.write(f"Predicted Iris Species: {species_mapping[prediction]}")
